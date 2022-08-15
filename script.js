770ccf5614bac2b35375e52e9cad5444df0e9882
Sergey <77577095+First1by@users.noreply.github.com>
2022-08-15 16:12:53 +0300

    Upd

--- script.js
+++ script.js
@@ -9,9 +9,12 @@ const greeting = document.querySelector('.greeting');
 const timeOfDay = getTimeOfDay();
 const greetingText = `Good ${timeOfDay}`;
 const name = document.querySelector('.name');
+let randomNum;
+const slideNext = document.querySelector('.slide-next');
+const slidePrev = document.querySelector('.slide-prev');
+
 
 showTime();
-
 
 function showTime() {
   time.innerHTML = new Date().toLocaleTimeString();
@@ -33,7 +36,7 @@ function getTimeOfDay() {
     	return 'morning'
     break;
     case 2:
-    	return 'day'
+    	return 'afternoon'
     break;
     case 3:
     	return 'evening'
@@ -58,3 +61,69 @@ function getLocalStorage() {
 window.addEventListener('load', getLocalStorage);
 
 
+let resultNumPicture = bgNum();
+
+function getRandomNum() { 
+  
+   randomNum = Math.floor(Math.random()*20);
+   return randomNum;
+}
+
+function bgNum() {
+    let resultNumRandom = getRandomNum();
+    if(resultNumRandom != 0) {
+        return String(resultNumRandom).padStart(2, "0");
+    } else {
+        return String(resultNumRandom + 1).padStart(2, "0");
+    }
+}
+
+setBg1();
+
+function setBg1() {
+    getRandomNum();
+    bgNum();
+    const img = new Image();
+img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum()}.jpg`;
+img.onload = () => {
+document.body.style.backgroundImage = `url(${img.src})`;
+	};
+    
+    setTimeout(setBg1, 3000);
+}
+
+function setBg() {
+  let a = '';
+
+  if(randomNum != 0) {
+    a = String(randomNum).padStart(2, "0");
+} else {
+     a = String(randomNum + 1).padStart(2, "0");
+}
+const img = new Image();
+img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${a}.jpg`;
+img.onload = () => {
+document.body.style.backgroundImage = `url(${img.src})`;
+	};
+}
+
+slideNext.addEventListener('click', getSlideNext);
+slidePrev.addEventListener('click', getSlidePrev);
+
+function getSlideNext() {
+    if(randomNum < 20) {
+      randomNum = randomNum + 1;
+    } else if (randomNum === 20) {
+      randomNum = 1;
+    }
+    setBg();
+}
+
+function getSlidePrev() {
+    if(randomNum > 1) {
+      randomNum = randomNum - 1;
+    } else if (randomNum === 0) {
+      randomNum = 20;
+    }
+  setBg();
+}
